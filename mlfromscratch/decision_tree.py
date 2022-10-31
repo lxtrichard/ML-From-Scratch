14bc1e634f266fddd8e1fca575ed3f41e427e3d1
lxtrichard <1092433085@qq.com>
2022-10-30 17:12:38 -0700

    add random forest

--- mlfromscratch/decision_tree.py
+++ mlfromscratch/decision_tree.py
@@ -2,11 +2,10 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import sys
-import os
+import progressbar
 
 from utils import accuracy_score, cal_entropy
-from misc import Plot
+from misc import Plot, bar_widgets
 
 class DecisionNode():
     def __init__(self, feature_i=None, threshold=None, value=None, true_branch=None, false_branch=None):
@@ -52,7 +51,7 @@ class DecisionTree():
         X_1 = np.array([sample for sample in X if split_func(sample)])
         X_2 = np.array([sample for sample in X if not split_func(sample)])
 
-        return np.array([X_1, X_2])
+        return np.array([X_1, X_2], dtype=object)
 
     def _build_tree(self, X, y, current_depth=0):
         largest_impurity = 0
@@ -136,6 +135,86 @@ class ClassificationTree(DecisionTree):
         self._impurity_calculation = self._calculate_information_gain
         self._leaf_value_calculation = self._majority_vote
         super(ClassificationTree, self).fit(X, y)
+        
+class RandomForest():
+    def __init__(self, n_estimators=100, max_features=None, min_samples_split=2,
+                 min_gain=0, max_depth=float("inf")):
+        self.n_estimators = n_estimators    # Number of trees
+        self.max_features = max_features    # Maxmimum number of features per tree
+        self.min_samples_split = min_samples_split
+        self.min_gain = min_gain            # Minimum information gain req. to continue
+        self.max_depth = max_depth          # Maximum depth for tree
+        self.progressbar = progressbar.ProgressBar(widgets=bar_widgets)
+
+        # Initialize decision trees
+        self.trees = []
+        for _ in range(n_estimators):
+            self.trees.append(
+                ClassificationTree(
+                    min_samples_split=self.min_samples_split,
+                    min_impurity=min_gain,
+                    max_depth=self.max_depth))
+
+    def get_random_subsets(self, X, y, n_subsets, replacements=True):
+        """ Return random subsets (with replacements) of the data """
+        n_samples = np.shape(X)[0]
+        # Concatenate x and y and do a random shuffle
+        X_y = np.concatenate((X, y.reshape((1, len(y))).T), axis=1)
+        np.random.shuffle(X_y)
+        subsets = []
+
+        # Uses 50% of training samples without replacements
+        subsample_size = int(n_samples // 2)
+        if replacements:
+            subsample_size = n_samples      # 100% with replacements
+
+        for _ in range(n_subsets):
+            idx = np.random.choice(
+                range(n_samples),
+                size=np.shape(range(subsample_size)),
+                replace=replacements)
+            X = X_y[idx][:, :-1]
+            y = X_y[idx][:, -1]
+            subsets.append([X, y])
+        return subsets
+    
+    def fit(self, X, y):
+        n_features = np.shape(X)[1]
+        # If max_features have not been defined => select it as
+        # sqrt(n_features)
+        if not self.max_features:
+            self.max_features = int(np.sqrt(n_features))
+
+        # Choose one random subset of the data for each tree
+        subsets = self.get_random_subsets(X, y, self.n_estimators)
+
+        for i in self.progressbar(range(self.n_estimators)):
+            X_subset, y_subset = subsets[i]
+            # Feature bagging (select random subsets of the features)
+            idx = np.random.choice(range(n_features), size=self.max_features, replace=True)
+            # Save the indices of the features for prediction
+            self.trees[i].feature_indices = idx
+            # Choose the features corresponding to the indices
+            X_subset = X_subset[:, idx]
+            # Fit the tree to the data
+            self.trees[i].fit(X_subset, y_subset)
+
+    def predict(self, X):
+        y_preds = np.empty((X.shape[0], len(self.trees)))
+        # Let each tree make a prediction on the data
+        for i, tree in enumerate(self.trees):
+            # Indices of the features that the tree has trained on
+            idx = tree.feature_indices
+            # Make a prediction based on those features
+            prediction = tree.predict(X[:, idx])
+            y_preds[:, i] = prediction
+            
+        y_pred = []
+        # For each sample
+        for sample_predictions in y_preds:
+            # Select the most common class prediction
+            y_pred.append(np.bincount(sample_predictions.astype('int')).argmax())
+        return y_pred
 
 
 if __name__ == "__main__":
@@ -146,6 +225,7 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
     clf = ClassificationTree()
+    # clf = RandomForest()
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
 
